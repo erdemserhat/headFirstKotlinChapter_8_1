8b70d25f0a28991c25d69c0d46e8aeb5dac53496
Serhat <me.serhaterdem@gmail.com>
2023-10-21 16:33:34 +0300

    -Elvis Operator Not-null assertion operator

---
+++ src/main/kotlin/JavaClass.java
@@ -0,0 +1,14 @@
+public class JavaClass {
+
+
+    //Elvis Operator In Java
+    public  static Boolean compare(int a, int b){
+        if(a>b) return  true;
+        else return false;
+    }
+    public static void main(String...args){
+        System.out.println( compare(4,4)  ? "serhat" : "erdem");
+    }
+
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,40 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //Setting default values for null values by using if expression
+
+    data class Password(
+        val passwordTitle: String? = "Example Password Title",
+        val passwordContent: String? = "Example Password Content"
+    )
+
+    var myInstagramPassword: Password? = Password("Instagram", "abcdefgh")
+
+   // myInstagramPassword = null
+    var a = if (myInstagramPassword != null) myInstagramPassword.passwordTitle else "12345"
+    //This is like saying, if myInstagramPassword is not null ;
+    //return myInstagramPassword.passwordTitle property
+    //otherwise return "12345"
+    //because myInstagramPassword is null, a is going to be assigned "12345"
+    println(a)
+
+    //Let's do the same thing by using Elvis operator
+
+    println(myInstagramPassword?.passwordContent?:"it was null")
+    //It is like saying if myInstagramPassword is not null and
+    //myInstagramPassword.passwordContent is not null return its value
+    //otherwise returns "it was null"
+    //it does same thing the code below;
+    println(if (myInstagramPassword!=null && myInstagramPassword.passwordContent!=null )
+        myInstagramPassword.passwordContent else "it was null"
+    )
+    //*   myInstagramPassword?.passwordContent?:"it was null"
+    //Basically  The Elvis Operator ?: is a safe version of an if expression.
+    //It returns the value of its left if that is not null. [myInstagramPassword.passwordContent]
+    //Otherwise , it returns the value on its right. ["it was null"]
+
+
+
+
+
+
 }
